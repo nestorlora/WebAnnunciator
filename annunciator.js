cefff3ba5bfbad4cdf8c6915d79221f8e2150b01
nestorlora <nestorlora@geeklab.es>
2023-06-18 20:10:34 +0200

    Added indicator blinking setting
    Signed-off-by: nestorlora <nestorlora@geeklab.es>

--- annunciator.js
+++ annunciator.js
@@ -1,4 +1,19 @@
-function indicator_on(id, blinking = false){
+// CONFIG
+
+var blinkOnAlarm = false;
+var blinkOnPutout = true;
+
+// INDIVIDUAL INDICATOR CONTROLS
+
+function indicator_on(id){
+    let element = document.getElementById(id);
+    element.classList.remove("sts-off");
+    if(blinkOnAlarm){
+        indicator_blink(id);
+    }
+}
+
+function indicator_on(id, blinking){
     let element = document.getElementById(id);
     element.classList.remove("sts-off");
     if(blinking){
@@ -6,7 +21,15 @@ function indicator_on(id, blinking = false){
     }
 }
 
-function indicator_off(id, blinking = false){
+function indicator_off(id){
+    let element = document.getElementById(id);
+    element.classList.add("sts-off");
+    if(blinkOnPutout){
+        indicator_blink(id);
+    }
+}
+
+function indicator_off(id, blinking){
     let element = document.getElementById(id);
     element.classList.add("sts-off");
     if(blinking){
@@ -24,4 +47,22 @@ function blink_click(){
     let element = this;
     element.classList.remove("blink");
     element.removeEventListener("click", blink_click);
+}
+
+// ANNUNCIATOR DASHBOARD CONTROLS
+
+function setBlinkOnAlarm(enable){
+    if(enable && typeof enable == "boolean"){
+        blinkOnAlarm = enable;
+    }else{
+        throw new Error("Value is not Boolean!");
+    }
+}
+
+function setBlinkOnPutout(enable){
+    if(enable && typeof enable == "boolean"){
+        blinkOnAlarm = enable;
+    }else{
+        throw new Error("Value is not Boolean!");
+    }
 }
